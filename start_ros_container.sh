3989855b6c8dbc6b0beda0c71b45716e39e2a615
Khalid Bourr <khalid.bourr@unicam.it>
2024-06-07 16:42:48 +0200

    test

---
+++ start_ros_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Allowing local connections to the X server
+xhost +SI:localuser:root
+
+# Command to run the Docker container with GPU and display settings
+docker run --gpus all -it \
+    --env="DISPLAY=$DISPLAY" \
+    --env="XDG_RUNTIME_DIR=/tmp" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --volume="ros-tools:/root" \
+    my-custom-ros:noetic \
+    /bin/bash
+
+# Optionally, reset the X server settings
+xhost -SI:localuser:root
+
